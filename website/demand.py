9d8fc3eefc66577b002c6e62bbbc5f8641beda1a
Darshan <123456dharshan@gmail.com>
2022-06-15 15:41:37 +0530

    demand_graph

--- website/demand.py
+++ website/demand.py
@@ -3,16 +3,37 @@ from .models import Stats
 from sqlalchemy import create_engine
 import sqlalchemy
 import sqlite3
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pandas as pd
 
 def demand_graph(book):
     stats = Stats.query.filter_by(book_id=book.id).all()
-    df = pd.DataFrame(columns=['id','week_stamp','book_id','demand_time','dt','number_of_issues','number_notify_me','unique_visits','demand','number_of_copies'])
+    df = pd.DataFrame(columns=['id','week_stamp','book_id','demand_time','dt','number_of_issues','number_notify_me','unique_visits','Demand','number_of_copies'])
     df1 = pd.DataFrame(columns=['name','age','water'])
     print(df)
     # df = pd.DataFrame(stats[0],columns=['id','week_stamp','book_id','demand_time','dt','number_of_issues','number_notify_me','unique_visits','demand','number_of_copies'])
     i = 0
+    print(stats)
     for stat in stats:
-        df.loc[i] = [stat.id, stat.week_stamp, stat.book_id,stat.demand_time,stat.dt,stat.number_of_issues,stat.number_notify_me,stat.unique_visits,stat.demand,stat.number_of_copies] 
+        print(stat.week_stamp)
+        df.loc[i] = [stat.id, stat.week_stamp, stat.book_id,stat.demand_time,stat.dt,stat.number_of_issues,stat.number_notify_me,stat.unique_visits,stat.demand,stat.unique_visits] 
+        print(df)
         i=i+1
     print(df)
+
+    plot_df = df[['week_stamp','number_of_copies','Demand']]
+
+    demand_mean = [np.mean(df.Demand)]*len(plot_df)
+    demand_mean_value = plot_df['Demand'].rolling(window=12).mean()
+
+    nc_mean_value = plot_df['number_of_copies'].rolling(window=12).mean()
+
+    plt.plot(plot_df.Demand, label = "Demand")
+    plt.plot(plot_df.number_of_copies, label = "Number of Copies")
+    plt.plot(demand_mean_value, label = "Demand Mean Curve")
+    plt.plot(nc_mean_value, label = "Available Copies Mean Curve")
+
+    plt.legend()
+    plt.show()
